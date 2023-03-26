5edeb42a6711a3d92d6d4b6a8c30dc6220bda5b0
faisalalbarkah1414 <faisalalbarkah1414@gmail.com>
2023-03-26 16:45:37 +0700

    Initial Commit

---
+++ FoodMarketiOS/FoodMarketiOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  FoodMarketiOSApp.swift
+//  FoodMarketiOS
+//
+//  Created by faisalalbarkah on 26/03/23.
+//
+
+import SwiftUI
+
+@main
+struct FoodMarketiOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
